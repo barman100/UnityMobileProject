ee75e06aa2388d001001558c7289ae388623a0a3
SummerNova <137454229+SummerNova@users.noreply.github.com>
2023-10-01 02:03:19 +0300

    Level Selection Screens Exist, still no level starting functionality

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -1,10 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenuManager : MonoBehaviour
 {
     //This is the Main Menu Manager - responsible for transitioning us into the level selection.
+    [SerializeField] GameObject mainMenuCanvas;
+    [SerializeField] GameObject LevelSelectionCanvas;
+    [SerializeField] GameObject[] LevelSelectionPanels;
+    [SerializeField] Button[] LevelSelectionButtons;
+    [SerializeField] int selectedLevelSet;
 
     //This function will start the game at your latest available level
     public void StartGame()
@@ -16,6 +22,15 @@ public class MainMenuManager : MonoBehaviour
     public void SelectStage()
     {
         Debug.Log("Level Selection Screen Initiated.");
+        mainMenuCanvas.SetActive(false);
+        LevelSelectionCanvas.SetActive(true);
+    }
+
+    public void GoToMainMenu()
+    {
+        Debug.Log("Returned to Main Menu.");
+        mainMenuCanvas.SetActive(true);
+        LevelSelectionCanvas.SetActive(false);
     }
     
     //This function will close the game
@@ -24,6 +39,15 @@ public class MainMenuManager : MonoBehaviour
         Debug.Log("Aplication Closing.");
         Application.Quit();
 
+    }
+
+    public void SelectLevelSet(int setID)
+    {
+        LevelSelectionPanels[selectedLevelSet].SetActive(false);
+        LevelSelectionButtons[selectedLevelSet].interactable = true;
+        selectedLevelSet = setID;
+        LevelSelectionPanels[selectedLevelSet].SetActive(true);
+        LevelSelectionButtons[selectedLevelSet].interactable = false;
     }
 
 
