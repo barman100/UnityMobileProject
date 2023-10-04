205da5fabb98eb94e05230e1fb19a0b5874eb318
RonCohen47 <138518157+RonCohen47@users.noreply.github.com>
2023-10-04 23:15:35 +0300

    Tweening

--- Assets/Scripts/CollectGem.cs
+++ Assets/Scripts/CollectGem.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using DG.Tweening;
 
 public class CollectGem : MonoBehaviour
 {
@@ -9,7 +10,17 @@ public class CollectGem : MonoBehaviour
     private ParticleSystem _partical;
     [SerializeField]
     private GameObject _diamondChild;
+    [SerializeField] MeshRenderer DiamondMeshRendrer;
     private bool NotDestroyed = false;
+
+    [SerializeField] Color Color1;
+    [SerializeField] Color Color2;
+    Color newColor;
+
+    public Vector3 EndValueRotation = new Vector3(0,360,0);
+    public Vector3 EndValueScales= new Vector3(0.2f, 0.2f, 0.2f);
+
+    public bool SwitchedColor = true;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (!NotDestroyed && collision.gameObject.tag == "player")
@@ -18,6 +29,21 @@ public class CollectGem : MonoBehaviour
             _partical.Play();
             _diamondChild.SetActive(false);
             Destroy(this.gameObject, 1f);
+        }
+    }
+
+    private void Update()
+    {
+        if (DOTween.PlayingTweens() == null)
+        {
+            if (SwitchedColor) { newColor = Color1; SwitchedColor = false; }
+            else { newColor = Color2; SwitchedColor = true; }
+
+            EndValueRotation = EndValueRotation * -1;
+            EndValueScales = EndValueScales * -1;
+            transform.DOLocalRotate(EndValueRotation, 5f, RotateMode.Fast);
+            transform.DOScale(Vector3.one - EndValueScales , 5f);
+            DiamondMeshRendrer.material.DOColor(newColor, 5f); 
         }
     }
 }
